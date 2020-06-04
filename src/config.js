44c75c027f10b592a247673886ad9d72613892c6
antonioscardovi <antonio.scardovi@gmail.com>
2020-06-04 15:40:03 +0200

    Setting up React App

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "us-east-1",
+      BUCKET: "ascardovi-serverless-test"
+    },
+    apiGateway: {
+      REGION: "us-east-1",
+      URL: "https://5ey0uiqmdh.execute-api.us-east-1.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "us-east-1",
+      USER_POOL_ID: "us-east-1_OcRgnnxdF",
+      APP_CLIENT_ID: "6mafrmh6r9nk37gc5p567eq54o",
+      IDENTITY_POOL_ID: "us-east-1:5b8a9bf0-4f42-4b51-80e2-9cdbecd149ca"
+    }
+  };
